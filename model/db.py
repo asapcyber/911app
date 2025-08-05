6a3d55dbb95a2a084d9ae02612ba11369f24a19b
Massimo Rapparini <massimo.rapparini@gmail.com>
2025-08-05 05:48:14 -0700

    Add SQLAlchemy CallRecord model

---
+++ model/db.py
@@ -0,0 +1,12 @@
+# model/db.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./112_calls.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
